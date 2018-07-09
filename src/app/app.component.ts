9dc7fa30c91aa2bcb1e186ccbd673e1d0003b64d
Nick Winner <design@winnerdigital.net>
2018-07-09 11:49:08 -0400

    Replace images with Hi-Res, implement name delimiter. Add/remove bios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,12 +9,17 @@ import { Member } from 'environments/types';
 })
 export class AppComponent {
   modal = false;
+  modalDom = false;
   windowWidth: number;
   activeMember: Member;
   team: Member[];
 
   @HostListener('window:resize', ['$event']) handleResize(event) {
     this.windowWidth = event.target.innerWidth;
+
+    if (this.modal && this.isMobile) {
+      // window.scrollTo(0, 0);
+    }
   }
 
   @HostListener('window:keyup', ['$event']) handleKeyup(event: KeyboardEvent) {
@@ -29,6 +34,30 @@ export class AppComponent {
     this.teamService.getTeam().then(team => {
       this.team = team;
     });
+
+    // set window width
+    this.windowWidth = window.innerWidth;
+  }
+
+  showModal(member: Member) {
+    this.activeMember = member;
+    this.modalDom = true;
+
+    setTimeout(() => {
+      this.modal = true;
+    }, 100);
+
+    if (this.isMobile) {
+      window.scrollTo(0, 0);
+    }
+  }
+
+  hideModal() {
+    this.modal = false;
+
+    setTimeout(() => {
+      this.modalDom = false;
+    }, 250);
   }
 
   get isMobile(): boolean {
